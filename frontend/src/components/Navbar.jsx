9d293b098a42b4a8cd5af73d859e0ea3aa966f9b
catkhanhnguyen123 <khanh.nguyenlc@hcmut.edu.vn>
2023-04-23 19:24:38 +0700

    product

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
                 <i className="fa fa-user-plus me-1"></i>Register
               </a>
               <a href="/cart" className="btn btn-outline-dark ms-2">
-                <i className="fa fa-shopping-cart me-1"></i>Cart (0)
+                <i className="fa fa-shopping-cart me-1"></i>Cart
               </a>
             </div>
           </div>
